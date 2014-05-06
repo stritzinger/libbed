0ef162d1063503a6e047381e2a29314dd3503021
Sebastian Huber <sebastian.huber@embedded-brains.de>
2014-05-06 10:55:04 +0200

    Print a summary block status

--- bed-print-bad-blocks.c
+++ bed-print-bad-blocks.c
@@ -22,12 +22,16 @@ void bed_print_bad_blocks(const bed_partition *part, bed_printer printer, void *
 	bed_address address = 0;
 	bed_device *bed = part->bed;
 	uint32_t block_size = bed->block_size;
+	uint32_t bad_block_count = 0;
+	uint32_t valid_block_count = 0;
 
 	do {
 		status = bed_is_block_valid(part, address);
 		if (status == BED_ERROR_BLOCK_IS_BAD) {
 			uint32_t block = (address >> bed->block_shift) & (bed->block_size - 1);
 			uint32_t chip = address >> bed->chip_shift;
+
+			++bad_block_count;
 
 			(*printer)(
 				printer_arg,
@@ -37,8 +41,18 @@ void bed_print_bad_blocks(const bed_partition *part, bed_printer printer, void *
 				block
 			);
 			status = BED_SUCCESS;
+		} else if (status == BED_SUCCESS) {
+			++valid_block_count;
 		}
 
 		address += block_size;
 	} while (status == BED_SUCCESS);
+
+	(*printer)(
+		printer_arg,
+		"total blocks = %" PRIu32 ", bad blocks = %" PRIu32 ", valid blocks = %" PRIu32 "\n",
+		bad_block_count + valid_block_count,
+		bad_block_count,
+		valid_block_count
+	);
 }
